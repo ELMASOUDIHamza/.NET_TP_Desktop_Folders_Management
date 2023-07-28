d4a25e4e573d047fd4702522d8caa99c296025c4
Hamza <elmasoudihamzajob@gmail.com>
2023-07-28 14:51:29 +0100

    Pushing project to server

---
+++ FoldersManagement/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace FoldersManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCIN = new System.Windows.Forms.TextBox();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.txtPrenom = new System.Windows.Forms.TextBox();
+            this.txtSalaire = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.btnAfficher = new System.Windows.Forms.Button();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // txtCIN
+            // 
+            this.txtCIN.Location = new System.Drawing.Point(74, 107);
+            this.txtCIN.Name = "txtCIN";
+            this.txtCIN.Size = new System.Drawing.Size(100, 20);
+            this.txtCIN.TabIndex = 0;
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(74, 149);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(100, 20);
+            this.txtNom.TabIndex = 1;
+            // 
+            // txtPrenom
+            // 
+            this.txtPrenom.Location = new System.Drawing.Point(74, 190);
+            this.txtPrenom.Name = "txtPrenom";
+            this.txtPrenom.Size = new System.Drawing.Size(100, 20);
+            this.txtPrenom.TabIndex = 2;
+            // 
+            // txtSalaire
+            // 
+            this.txtSalaire.Location = new System.Drawing.Point(74, 232);
+            this.txtSalaire.Name = "txtSalaire";
+            this.txtSalaire.Size = new System.Drawing.Size(100, 20);
+            this.txtSalaire.TabIndex = 3;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(218, 107);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(367, 145);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Location = new System.Drawing.Point(218, 271);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(75, 23);
+            this.btnAjouter.TabIndex = 5;
+            this.btnAjouter.Text = "Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            this.btnAjouter.Click += new System.EventHandler(this.btnAjouter_Click);
+            // 
+            // btnAfficher
+            // 
+            this.btnAfficher.Location = new System.Drawing.Point(317, 271);
+            this.btnAfficher.Name = "btnAfficher";
+            this.btnAfficher.Size = new System.Drawing.Size(75, 23);
+            this.btnAfficher.TabIndex = 6;
+            this.btnAfficher.Text = "Afficher";
+            this.btnAfficher.UseVisualStyleBackColor = true;
+            this.btnAfficher.Click += new System.EventHandler(this.btnAfficher_Click);
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.Location = new System.Drawing.Point(413, 271);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(75, 23);
+            this.btnModifier.TabIndex = 7;
+            this.btnModifier.Text = "Modifier";
+            this.btnModifier.UseVisualStyleBackColor = true;
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.Location = new System.Drawing.Point(510, 271);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(75, 23);
+            this.btnSupprimer.TabIndex = 8;
+            this.btnSupprimer.Text = "Supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = true;
+            this.btnSupprimer.Click += new System.EventHandler(this.btnSupprimer_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "CIN";
+            this.columnHeader1.Width = 78;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Nom";
+            this.columnHeader2.Width = 95;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Prénom";
+            this.columnHeader3.Width = 89;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Salaire";
+            this.columnHeader4.Width = 177;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSupprimer);
+            this.Controls.Add(this.btnModifier);
+            this.Controls.Add(this.btnAfficher);
+            this.Controls.Add(this.btnAjouter);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.txtSalaire);
+            this.Controls.Add(this.txtPrenom);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.txtCIN);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCIN;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.TextBox txtPrenom;
+        private System.Windows.Forms.TextBox txtSalaire;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.Button btnAfficher;
+        private System.Windows.Forms.Button btnModifier;
+        private System.Windows.Forms.Button btnSupprimer;
+    }
+}
+
---
+++ FoldersManagement/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO; //Input Output
+
+namespace FoldersManagement
+{
+    
+    public partial class Form1 : Form
+    {
+        FileStream fileStream, fileStreamTemp;
+        StreamReader streamReader;
+        StreamWriter streamWriter;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAjouter_Click(object sender, EventArgs e)
+        {
+            fileStream = new FileStream(@"D:\fichier1.txt", FileMode.Append, FileAccess.Write); //Append = Ajouter
+            streamWriter = new StreamWriter(fileStream);
+            string ligne = txtCIN.Text + " - " + txtNom.Text + " - " + txtPrenom.Text +
+                            " - " + txtSalaire.Text + " Dhs. ";
+            streamWriter.WriteLine(ligne);
+            streamWriter.Close();
+            fileStream.Close();
+            MessageBox.Show("Ajout effecuté avec succès !");
+
+            txtCIN.Clear();
+            txtNom.Clear();
+            txtPrenom.Clear();
+            txtSalaire.Clear();
+        }
+
+        private void btnAfficher_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            if (File.Exists(@"D:\fichier1.txt"))
+            {
+                fileStream = new FileStream(@"D:\fichier1.txt", FileMode.Open, FileAccess.Read);
+                streamReader = new StreamReader(fileStream);
+                string line = streamReader.ReadLine();
+                while (line != null)
+                {
+                    string[] table = line.Split('-');
+                    listView1.Items.Add(new ListViewItem (table)); //String [] { table[0], table[1], table[2], table[3] }));
+                    line = streamReader.ReadLine();
+
+                }
+                streamReader.Close();
+                fileStream.Close(); 
+            }
+            else MessageBox.Show("Fichier inexistant !");
+
+
+        }
+
+        private void btnSupprimer_Click(object sender, EventArgs e)
+        {
+            fileStream = new FileStream(@"D:\fichier1.txt", FileMode.Open, FileAccess.Read);
+            fileStreamTemp = new FileStream(@"D:\fichierTemp.txt", FileMode.Append, FileAccess.Write);
+            streamReader = new StreamReader(fileStream);
+            streamWriter = new StreamWriter(fileStreamTemp);
+            bool supprime = false;
+            string ligne = streamReader.ReadLine();
+
+            while (ligne != null)
+            {
+                string[] table = ligne.Split('-');
+                if (txtCIN.Text == table[0]) streamWriter.WriteLine(ligne);
+                else
+                {
+                    supprime = true;
+                    ligne = streamReader.ReadLine();
+                }
+            }
+            streamWriter.Close();
+            streamReader.Close();
+            fileStream.Close();
+            fileStreamTemp.Close();
+            File.Delete(@"D:\fichier1.txt");
+            File.Move(@"D:\fichierTemp.txt", @"D:\fichier1.txt");
+
+            if (supprime == false)       MessageBox.Show("Employé inexistant !");
+            else            MessageBox.Show("Employé supprimé avec succès !");
+            
+        }
+    }
+}
